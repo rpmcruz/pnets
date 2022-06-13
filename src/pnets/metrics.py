3943bbbfb30be41e4d06bf95cab601aff5e4254f
Ricardo Cruz <rpcruz@fe.up.pt>
2022-06-14 00:05:33 +0100

    Added accuracy to the metrics. Added assertion to check if ypred are probabilities.

--- src/pnets/metrics.py
+++ src/pnets/metrics.py
@@ -1,8 +1,12 @@
 import torch
 
+def accuracy(ypred, y):
+    return (ypred == y).mean()
+
 def IoU(ypred, y, K):
     assert len(ypred.shape) == 1, 'ypred must be a vector'
     assert len(y.shape) == 1, 'y must be a vector'
+    assert torch.all(torch.logical_and(ypred >= 0, ypred < K)), 'ypred must be probabilities (did you forget the softmax?)'
     ypred = ypred[None]
     y = y[None]
     kk = torch.arange(K, device=y.device)[:, None]
