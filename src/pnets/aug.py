Fixed normalization bug. Added val fold to kitti semantic.

src/pnets/aug.py
@@ -40,8 +40,8 @@ def Compose(*l):
 def Normalize():
     ''' Normalization to [0,1]. Formula: `(P-center)/max_value`. '''
     def f(P, S):
-        center = np.mean(P, 0, keepdims=True)
-        max_value = np.max(np.abs(P-center), 0, keepdims=True)
+        center = np.mean(P, 1, keepdims=True)
+        max_value = np.max(np.abs(P-center), 1, keepdims=True)
         P = (P-center)/max_value
         return P, S
     return f